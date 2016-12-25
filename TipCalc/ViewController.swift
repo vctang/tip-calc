89d5b3c552617995cf58d91c0bde3e47fc282017
Vicky Tang <VickyTang@vickys-mbp-2.attlocal.net>
2016-12-25 13:34:33 -0800

    Post ignore and attributes

--- TipCalc/ViewController.swift
+++ TipCalc/ViewController.swift
@@ -10,9 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var splitLabel: UILabel!
+    @IBOutlet weak var decrementButton: UIButton!
+    @IBOutlet weak var incrementButton: UIButton!
+    
+    //Properties
+    var split = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.billField.becomeFirstResponder()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +32,41 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    //Dismiss keyboard when you tap on the screen
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func splitDec(sender: UIButton) {
+        if split - 1 >= 1 {
+            split = split - 1
+            updateSplitLbl()
+            calculateTip(UIButton)
+        }
+    }
+    
+    @IBAction func splitInc(sender: UIButton) {
+        split = split + 1
+        updateSplitLbl()
+        calculateTip(UIButton)
+    }
+    
+    func updateSplitLbl() {
+        splitLabel.text = "\(split)"
+    }
+    
+    @IBAction func calculateTip(sender: AnyObject) {
+        let tipPercentages = [0.15, 0.2, 0.25]
+        
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = (bill + tip) / Double(split)
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
 
+    }
+    
 }
+
 
